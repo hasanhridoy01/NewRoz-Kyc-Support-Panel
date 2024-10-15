e7bcc5c6328417df15b705518ce9a977bcdd271d
hasanhridoy01 <hasanhridoy1367@gmail.com>
2024-10-15 15:04:55 +0600

    set layout

--- src/layout/Main/Main.jsx
+++ src/layout/Main/Main.jsx
@@ -314,17 +314,7 @@ const Main = () => {
 
         {/* Apply Outlet */}
         <div className="outlet-style">
-          <Container
-            maxWidth={false} // Disable default maxWidth handling
-            sx={{
-              maxWidth: "1340px", // Set custom max-width
-              width: "100%",
-              height: "100%", // Ensure it takes full width if needed
-              margin: "auto", // Center the container
-            }}
-          >
             <Outlet />
-          </Container>
         </div>
 
         {/* NotificationDrawer */}
